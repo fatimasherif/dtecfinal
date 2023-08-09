8b62feba6b3deb22a6b676467fd5d5847d26a75b
fatoma <fatmasherif20022@gmail.com>
2023-08-09 15:24:32 +0300

    fixed

--- src/Components/Company/Company.js
+++ src/Components/Company/Company.js
@@ -5,21 +5,21 @@ import finalvid from "../../assests/finalvid.mp4";
 const Company = () => {
   return (
     <div className=" bg-gradient-to-br from-blue-200 to-blue-600">
-      <div className="container ">
-        <div className="">
-          <h2 className="  border p-3 w-[300px]   shadow-lg bg-blue-100 ml-[500px]  font-bold text-blue-900  mt-[40px] mb-6">
+      <div className="container  items-center p-24 ">
+        <div className="items-center text-center pt-2">
+          <p className="text-white text-4xl mt-10 mb-6 px-6 font-bold  p-5  border border-r-amber-300 shadow-2xl rounded-lg  max-w-[90%] mx-auto">
             Our History
-          </h2>
-        </div>
-        <div>
-          <video
-            src={finalvid}
-            autoPlay
-            loop
-            muted
-            controls={false}
-            className="w-[1000px] h-auto mx-auto object-cover"
-          />
+          </p>
+          <div>
+            <video
+              src={finalvid}
+              autoPlay
+              loop
+              muted
+              controls={false}
+              className="w-full h-auto mx-auto object-cover"
+            />
+          </div>
         </div>
       </div>
     </div>
